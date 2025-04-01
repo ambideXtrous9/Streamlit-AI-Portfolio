187a99c7f9e5a23f4a27484b277994fa95415bcf
Sushovan Saha <31372586+ambideXtrous9@users.noreply.github.com>
2025-04-01 11:28:51 +0530

    RuntimeError: Tried to instantiate class '__path__._path', but it does not exist!

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import torch 
+import os
+
+torch.classes.__path__ = [os.path.join(torch.__path__[0], torch.classes.__file__)] 
+
+# or simply:
+# torch.classes.__path__ = []
 
 from sidebar import SideBar
 from navigate import navigator
