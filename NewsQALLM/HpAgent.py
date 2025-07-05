4b3a91b97c0252910a2305738e6107a716659eba
ssgrassstone <sushovan.saha@grassstone.io>
2025-07-06 00:49:10 +0530

    key change

--- NewsQALLM/HpAgent.py
+++ NewsQALLM/HpAgent.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 
 llm = ChatGroq(
